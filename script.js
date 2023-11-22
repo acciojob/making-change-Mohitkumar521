b89caa9042afc1ec49752a8cadd26db9224dd843
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-22 13:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 const makeChange = (c) => {
-  // your name here
+  // Initialize variables to count each type of coin
+  let quarters = 0;
+  let dimes = 0;
+  let nickels = 0;
+  let pennies = 0;
+
+  // Convert input to a number
+  const amount = Number(c);
+
+  // Calculate the number of quarters
+  quarters = Math.floor(amount / 25);
+  // Update the amount after using quarters
+  amount %= 25;
+
+  // Calculate the number of dimes
+  dimes = Math.floor(amount / 10);
+  // Update the amount after using dimes
+  amount %= 10;
+
+  // Calculate the number of nickels
+  nickels = Math.floor(amount / 5);
+  // Update the amount after using nickels
+  amount %= 5;
+
+  // The remaining amount is the number of pennies
+  pennies = amount;
+
+  // Create and return the result object
+  const result = {
+    q: quarters,
+    d: dimes,
+    n: nickels,
+    p: pennies,
+  };
+
+  return result;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
+
